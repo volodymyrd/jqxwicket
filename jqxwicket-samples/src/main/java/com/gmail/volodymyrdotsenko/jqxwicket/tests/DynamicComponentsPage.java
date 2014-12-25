a73d137bd29ce1cf0252e6e960af40559f7d0f9b
volodymyrd <volodymyrdotsenko@gmail.com>
2014-12-25 11:40:24 +0200

    create comp dynamic

--- jqxwicket-samples/src/main/java/com/gmail/volodymyrdotsenko/jqxwicket/tests/DynamicComponentsPage.java
+++ jqxwicket-samples/src/main/java/com/gmail/volodymyrdotsenko/jqxwicket/tests/DynamicComponentsPage.java
@@ -1,59 +1,72 @@
 package com.gmail.volodymyrdotsenko.jqxwicket.tests;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
-import org.apache.wicket.Component;
 import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.util.io.IClusterable;
 
 import com.gmail.volodymyrdotsenko.jqxwicket.widgets.buttons.XButton;
-import com.gmail.volodymyrdotsenko.jqxwicket.widgets.tabs.IXTab;
+
+interface IDynCompModel extends IClusterable {
+}
 
 public class DynamicComponentsPage extends WebPage {
 
 	private static final long serialVersionUID = 1L;
 
-	private ListView lw;
+	private final WebMarkupContainer wmc = new WebMarkupContainer("wmc");
+	private final List<IDynCompModel> componentlList = new ArrayList<IDynCompModel>();
 
 	public DynamicComponentsPage() {
 		XButton b = new XButton("but") {
+
+			private static final long serialVersionUID = 1L;
+
 			@Override
 			public void onClick(AjaxRequestTarget target) {
 				// target.appendJavaScript("alert('click')");
 				super.onClick(target);
 
-				
-				target.add(lw);
-				// DynamicComponentsPage.this.add(new AutoMarkupGenPanel("q"));
+				componentlList.add(new IDynCompModel() {
+					private static final long serialVersionUID = 1L;
+				});
+
+				target.add(wmc);
 			}
 		};
 
 		add(b);
 
-		List<AutoMarkupGenPanel> components = new ArrayList<AutoMarkupGenPanel>();
-		// components.add( new AutoMarkupGenPanel() );
-		// components.add( new AutoMarkupGenPanel() );
-		lw = new ListView<AutoMarkupGenPanel>("components", getModelObject()) {
-			protected void populateItem(ListItem item) {
-				item.add((AutoMarkupGenPanel) item.getModelObject());
+		ListView<IDynCompModel> lw = new ListView<IDynCompModel>("components",
+				getModelObject()) {
+
+			private static final long serialVersionUID = 1L;
+
+			protected void populateItem(ListItem<IDynCompModel> item) {
+				Label link = DynamicComponentsPage.this.newTitleLabel("link",
+						"link");
+				item.add(link);
 			}
 		};
 
-		add(lw);
+		lw.setOutputMarkupId(true);
+		wmc.setOutputMarkupId(true);
+		add(wmc);
+		wmc.add(lw);
 	}
 
-	public List<AutoMarkupGenPanel> getModelObject() {
-		List<AutoMarkupGenPanel> list = (List<AutoMarkupGenPanel>) this
-				.getDefaultModelObject();
+	protected Label newTitleLabel(String id, String title) {
+		return new Label(id, title);
+	}
 
-		if (list != null) {
-			return list;
-		}
+	public List<IDynCompModel> getModelObject() {
 
-		return Collections.emptyList();
+		return componentlList;
 	}
 }
